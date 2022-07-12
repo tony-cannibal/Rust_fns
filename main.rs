bd433a2ddfb95fcc18d09de44302fcdc2ed5ad81
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2022-07-12 11:48:58 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,59 @@
+
+use std::io;
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim().parse::<$type>().expect("Parsable");
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_str {
+    ($out:ident) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim();
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_vec {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).unwrap();
+        let $out = inner
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<$type>().unwrap())
+            .collect::<Vec<$type>>();
+    };
+}
+
+fn main() {
+  
+    read!(x as u32);
+    println!("{}", x);
+    
+}
+
+// A function that reads numbers
+#[allow(dead_code)]
+fn read_number() -> u32 {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => return i,
+        Err(..) => return 0,
+    };
+    
+}
+
+
+
